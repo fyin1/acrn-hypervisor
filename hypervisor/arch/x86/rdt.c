99570e7e2d294a19f7f1dc394d5623676eb2a165
Vijay Dhanraj <vijay.dhanraj@intel.com>
2020-02-17 12:50:19 -0800

    HV: Generalize RDT infrastructure and fix RDT cache configuration.
    This patch creates a generic infrastructure for
    RDT resources instead of just L2 or L3 cache. This
    patch also fixes L3 CAT config overwrite by L2 in
    cases where both L2 and L3 CAT are supported.
    
    Tracked-On: #3715
    Signed-off-by: Vijay Dhanraj <vijay.dhanraj@intel.com>

--- hypervisor/arch/x86/rdt.c
+++ hypervisor/arch/x86/rdt.c
@@ -12,80 +12,149 @@
 #include <errno.h>
 #include <logmsg.h>
 #include <rdt.h>
+#include <bits.h>
 #include <board.h>
 #include <vm_config.h>
 #include <msr.h>
 
-struct cat_hw_info cat_cap_info = {0};
+struct rdt_hw_info res_cap_info[RDT_NUM_RESOURCES] = {
+	[RDT_RESOURCE_L3] = {
+		.supported = false,
+		.bitmask = 0,
+		.cbm_len = 0,
+		.clos_max = 0,
+		.res_id = RDT_RESID_L3
+	},
+	[RDT_RESOURCE_L2] = {
+		.supported = false,
+		.bitmask = 0,
+		.cbm_len = 0,
+		.clos_max = 0,
+		.res_id = RDT_RESID_L2
+	},
+};
 const uint16_t hv_clos = 0U;
 uint16_t platform_clos_num = MAX_PLATFORM_CLOS_NUM;
 
-#ifdef	CONFIG_CAT_ENABLED
-int32_t init_cat_cap_info(void)
+#ifdef	CONFIG_RDT_ENABLED
+static void rdt_read_cache_alloc_cap(int res)
 {
+	uint32_t eax = 0U, ebx = 0U, ecx = 0U, edx = 0U;
+
+	/* CPUID.(EAX=0x10,ECX=ResID):EAX[4:0] reports the length of CBM supported
+	 * CPUID.(EAX=0x10,ECX=ResID):EBX[31:0] indicaterdt_read_cache_alloc_caps the corresponding uints
+	 * may be used by other entities such as graphic and H/W outside processor.
+	 * CPUID.(EAX=0x10,ECX=ResID):EDX[15:0] reports the maximun CLOS supported
+	 */
+	cpuid_subleaf(CPUID_RDT_ALLOCATION, res_cap_info[res].res_id, &eax, &ebx, &ecx, &edx);
+	res_cap_info[res].cbm_len = (uint16_t)((eax & 0xfU) + 1U);
+	res_cap_info[res].bitmask = ebx;
+	res_cap_info[res].clos_max = (uint16_t)(edx & 0xffffU) + 1;
+}
+
+int32_t init_rdt_cap_info(void)
+{
+	uint8_t i;
 	uint32_t eax = 0U, ebx = 0U, ecx = 0U, edx = 0U;
 	int32_t ret = 0;
 
-	if (pcpu_has_cap(X86_FEATURE_CAT)) {
-		cpuid_subleaf(CPUID_RSD_ALLOCATION, 0, &eax, &ebx, &ecx, &edx);
-		/* If support L3 CAT, EBX[1] is set */
+	if (pcpu_has_cap(X86_FEATURE_RDT_A)) {
+		cpuid_subleaf(CPUID_RDT_ALLOCATION, 0, &eax, &ebx, &ecx, &edx);
+
+		/* If HW supports L3 CAT, EBX[1] is set */
 		if ((ebx & 2U) != 0U) {
-			cat_cap_info.enabled = true;
-			cat_cap_info.res_id = CAT_RESID_L3;
+			res_cap_info[RDT_RESOURCE_L3].supported = true;
+			rdt_read_cache_alloc_cap(RDT_RESOURCE_L3);
 		}
 
-		/* If support L2 CAT, EBX[2] is set */
+		/* If HW supports L2 CAT, EBX[2] is set */
 		if ((ebx & 4U) != 0U) {
-			cat_cap_info.enabled = true;
-			cat_cap_info.res_id = CAT_RESID_L2;
+			res_cap_info[RDT_RESOURCE_L2].supported = true;
+			rdt_read_cache_alloc_cap(RDT_RESOURCE_L2);
 		}
 
-		/* CPUID.(EAX=0x10,ECX=ResID):EAX[4:0] reports the length of CBM supported
-		 * CPUID.(EAX=0x10,ECX=ResID):EBX[31:0] indicates the corresponding uints
-		 * may be used by other entities such as graphic and H/W outside processor.
-		 *  CPUID.(EAX=0x10,ECX=ResID):EDX[15:0] reports the maximun CLOS supported
+		/* RDT features can support different numbers of CLOS.
+		 * Set  the lowers numerical clos value (platform_clos_num)
+		 * that is common between the resources as each resource's
+		 * clos max value to have consistent allocation.
 		 */
-		cpuid_subleaf(CPUID_RSD_ALLOCATION, cat_cap_info.res_id, &eax, &ebx, &ecx, &edx);
-		cat_cap_info.cbm_len = (uint16_t)((eax & 0x1fU) + 1U);
-		cat_cap_info.bitmask = ebx;
-		cat_cap_info.clos_max = (uint16_t)(edx & 0xffffU);
+		for (i = 0U; i < RDT_NUM_RESOURCES; i++) {
+			if (!res_cap_info[i].supported)
+				continue;
 
-		if ((platform_clos_num != 0U) && ((cat_cap_info.clos_max + 1U) != platform_clos_num)) {
-			pr_err("%s clos_max:%hu, platform_clos_num:%u\n",
-			 __func__, cat_cap_info.clos_max, platform_clos_num);
-			ret = -EINVAL;
+			if ((platform_clos_num == 0U) || (res_cap_info[i].clos_max < platform_clos_num)) {
+				pr_err("Invalid resource clos max:Res_ID=%d, platform_clos_max=%d, res_clos_max=%d\n",
+					    res_cap_info[i].res_id, platform_clos_num, res_cap_info[i].clos_max);
+				ret = -EINVAL;
+				break;
+			}
+			res_cap_info[i].clos_max = platform_clos_num;
 		}
 	}
 
 	return ret;
 }
 
-void setup_clos(uint16_t pcpu_id)
+static bool setup_res_clos_msr(uint16_t pcpu_id, uint32_t res_id)
 {
+	bool ret = true;
 	uint16_t i;
 	uint32_t msr_index;
 	uint64_t val;
 
-	if (cat_cap_info.enabled) {
-		for (i = 0; i < platform_clos_num; i++) {
-			switch (cat_cap_info.res_id) {
-			case CAT_RESID_L2:
-				msr_index = platform_l2_clos_array[i].msr_index;
-				val = (uint64_t)platform_l2_clos_array[i].clos_mask;
-				msr_write_pcpu(msr_index, val, pcpu_id);
-				break;
-			case CAT_RESID_L3:
+	for (i = 0; i < platform_clos_num; i++) {
+		switch (res_id) {
+		case RDT_RESID_L3:
+			if ((fls32(platform_l3_clos_array[i].clos_mask) > res_cap_info[RDT_RESOURCE_L3].cbm_len) ||
+				(platform_l3_clos_array[i].msr_index != (MSR_IA32_L3_MASK_BASE + i))) {
+				ret = false;
+				pr_err("%s: Configure L3 CLOS Mask and MSR index in board.c correctly", __func__);
+			} else {
 				msr_index = platform_l3_clos_array[i].msr_index;
 				val = (uint64_t)platform_l3_clos_array[i].clos_mask;
 				msr_write_pcpu(msr_index, val, pcpu_id);
-				break;
-			default:
-				pr_err("Invalid RDT resource configuration\n");
 			}
+			break;
+		case RDT_RESID_L2:
+			if ((fls32(platform_l2_clos_array[i].clos_mask) > res_cap_info[RDT_RESOURCE_L2].cbm_len) ||
+				(platform_l2_clos_array[i].msr_index != (MSR_IA32_L2_MASK_BASE + i))) {
+				ret = false;
+				pr_err("%s: Configure L2 CLOS Mask and MSR index in board.c correctly", __func__);
+			} else {
+				msr_index = platform_l2_clos_array[i].msr_index;
+				val = (uint64_t)platform_l2_clos_array[i].clos_mask;
+				msr_write_pcpu(msr_index, val, pcpu_id);
+			}
+			break;
+		default:
+			pr_err("Invalid RDT resource configuration\n");
 		}
-		/* set hypervisor CAT clos */
+
+		if (ret != true)
+			break;
+	}
+
+	return ret;
+}
+
+bool setup_clos(uint16_t pcpu_id)
+{
+	bool ret = true;
+	uint16_t i;
+
+	for (i = 0U; i < RDT_NUM_RESOURCES; i++) {
+		if (!res_cap_info[i].supported) {
+			continue;
+		}
+		ret = setup_res_clos_msr(pcpu_id, res_cap_info[i].res_id);
+	}
+
+	if (ret != false) {
+		/* set hypervisor RDT resource clos */
 		msr_write_pcpu(MSR_IA32_PQR_ASSOC, clos2pqr_msr(hv_clos), pcpu_id);
 	}
+
+	return ret;
 }
 
 uint64_t clos2pqr_msr(uint16_t clos)
@@ -98,15 +167,14 @@ uint64_t clos2pqr_msr(uint16_t clos)
 	return pqr_assoc;
 }
 #else
-int32_t init_cat_cap_info(void)
+int32_t init_rdt_cap_info(void)
 {
-	cat_cap_info.enabled = false;
-
 	return 0U;
 }
 
-void setup_clos(__unused uint16_t pcpu_id)
+bool setup_clos(__unused uint16_t pcpu_id)
 {
+	return true;
 }
 
 uint64_t clos2pqr_msr(__unused uint16_t clos)
@@ -117,5 +185,12 @@ uint64_t clos2pqr_msr(__unused uint16_t clos)
 
 bool platform_rdt_support(void)
 {
-	return cat_cap_info.enabled;
+	bool ret = false;
+
+	if (res_cap_info[RDT_RESOURCE_L3].supported ||
+	    res_cap_info[RDT_RESOURCE_L2].supported) {
+		ret = true;
+	}
+
+	return ret;
 }
--- hypervisor/include/arch/x86/rdt.h
+++ hypervisor/include/arch/x86/rdt.h
@@ -7,26 +7,33 @@
 #ifndef RDT_H
 #define RDT_H
 
-/* The intel Resource Director Tech(RDT) based Cache Allocation Tech support */
-struct cat_hw_info {
-	bool enabled;		/* If L2/L3 CAT enabled */
+enum {
+	RDT_RESOURCE_L3,
+	RDT_RESOURCE_L2,
+
+	/* Must be the last */
+	RDT_NUM_RESOURCES,
+};
+
+/* The intel Resource Director Tech(RDT) based Allocation Tech support */
+struct rdt_hw_info {
+	bool supported;		/* If L2/L3 CAT supported */
 	uint32_t bitmask;	/* Used by other entities */
 	uint16_t cbm_len;	/* Length of Cache mask in bits */
 	uint16_t clos_max;	/* Maximum CLOS supported, the number of cache masks */
-
 	uint32_t res_id;
 };
 
-extern struct cat_hw_info cat_cap_info;
+
+extern struct rdt_hw_info res_cap_info[RDT_NUM_RESOURCES];
 extern const uint16_t hv_clos;
 extern uint16_t platform_clos_num;
-void setup_clos(uint16_t pcpu_id);
+bool setup_clos(uint16_t pcpu_id);
 
+#define RDT_RESID_L3   1U
+#define RDT_RESID_L2   2U
 
-#define CAT_RESID_L3   1U
-#define CAT_RESID_L2   2U
-
-int32_t init_cat_cap_info(void);
+int32_t init_rdt_cap_info(void);
 uint64_t clos2pqr_msr(uint16_t clos);
 bool platform_rdt_support(void);
 
